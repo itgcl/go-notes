96b10efe0a477610f318581745bf89210ea7efc8
PHPbulinbulin <44659541+PHPbulinbulin@users.noreply.github.com>
2020-04-19 20:37:14 +0800

    paging 和 单元测试

--- record/paging/paging.go
+++ record/paging/paging.go
@@ -1,12 +1,11 @@
 package paging
 
 import (
-"math"
+	"math"
 )
 
 type PagingInput struct {
-	Page int `json:"page"`  //当前页 (必须)
-	PageCount int `json:"page_count"` //总页数 (必须存在 pageCount 或者存在 pageSize 和 dataTotal)
+	Page int `json:"page"`	//当前页 (必须)
 	PageSize int `json:"page_size"`  //每页数量  (没有 pageCount 时候必须存在此数据)
 	DataTotal int `json:"data_total"` //总数据量  (没有 pageCount 时候必须存在此数据)
 	PrevBatch int `json:"prev_batch"` //显示page前多少页
@@ -19,7 +18,7 @@ type PagingOutput struct {
 	HasPaging bool `json:"has_paging"`//是否存在分页
 	PageCount int `json:"page_count"` //总页数
 	DataTotal int `json:"data_total"` //总数据量
-	Page int `json:"page"`  //当前页
+	Page int `json:"page"`	//当前页
 	IsFirstPage bool `json:"is_first_page"` //当前页是第一页
 	IsLastPage bool `json:"is_last_page"` //当前页是最后一页
 	PrevBatch []int `json:"prev_batch"` //当前页前几页  不存在前几页则为 [] (根据 传入的 prevPages 扩展)
@@ -40,23 +39,18 @@ func Paginate(data PagingInput) (output PagingOutput){
 		data.PageSize = 10
 	}
 	//没有count和total无法计算分页 直接返回
-	if data.PageCount < 1 && data.DataTotal < 1{
+	if  data.DataTotal < 1{
 		return
 	}
-	//当总页数不存在 通过总数据量获取
-	if data.PageCount < 1 {
-		data.PageCount = int(math.Ceil(float64(data.DataTotal) / float64(data.PageSize)))
-	}
-	//总页数
-	output.PageCount = data.PageCount
+	output.PageCount = int(math.Ceil(float64(data.DataTotal) / float64(data.PageSize)))
 
 	//page小于1
 	if data.Page < 1 {
 		data.Page = 1
 	}
 	//page大于pageCount
-	if data.Page > data.PageCount {
-		data.Page = data.PageCount
+	if data.Page > output.PageCount {
+		data.Page = output.PageCount
 	}
 	//当前页
 	output.Page = data.Page
@@ -71,16 +65,19 @@ func Paginate(data PagingInput) (output PagingOutput){
 		output.IsFirstPage = true
 	}
 	//page == pageCount 为最后一页
-	if data.Page == data.PageCount {
+	if data.Page == output.PageCount {
 		output.IsLastPage = true
 	}
+	//初始化数组
+	output.PrevBatch = []int{}
+	output.NextBatch = []int{}
 	//上一页  当前页不是第1页 并且总页数不是1
-	if data.Page != 1 && data.PageCount != 1 {
+	if data.Page != 1 && output.PageCount != 1 {
 		output.PrevPage = data.Page - 1
 	}else{
 		output.PrevPage = data.Page
 	}
-	if data.Page != data.PageCount {
+	if data.Page != output.PageCount {
 		output.NextPage = data.Page + 1
 	}else{
 		output.NextPage = data.Page
@@ -90,7 +87,7 @@ func Paginate(data PagingInput) (output PagingOutput){
 		output.PrevSomePage = data.Page - data.PrevSomePage
 		//防止 data.PrevSomePage > page
 		if output.PrevSomePage <= 0 {
-			output.PrevSomePage = data.Page
+			output.PrevSomePage = 1
 		}
 	}
 	//当前页后x页
@@ -98,7 +95,7 @@ func Paginate(data PagingInput) (output PagingOutput){
 		output.NextSomePage = data.Page + data.NextSomePage
 		//防止 data.PrevSomePage > PageCount
 		if output.NextSomePage > output.PageCount {
-			output.NextSomePage = data.PageCount
+			output.NextSomePage = output.PageCount
 		}
 	}
 	calculatePrevPage := data.Page - data.PrevBatch
@@ -123,25 +120,28 @@ func Paginate(data PagingInput) (output PagingOutput){
 		}
 	}
 	//当前页后[]页
-	calculateNextPage := data.Page + data.PrevBatch
+	calculateNextPage := data.Page + data.NextBatch
 
 	//传入参数大于总页数
-	if calculateNextPage > data.PageCount {
-		calculateNextPage = data.PageCount
+	if calculateNextPage > output.PageCount {
+		calculateNextPage = output.PageCount
 	}
 	//总页数 - 2 间隔1 有其他页
-	if data.PageCount - 2 >= calculateNextPage {
+	if output.PageCount - 2 >= calculateNextPage {
 		output.NextHasMorePage = true
 	}
 	//做过处理最大等于总页数 尾页必须显示 排除数组中
-	if calculateNextPage == data.PageCount {
+	if calculateNextPage == output.PageCount {
 		calculateNextPage -= 1
 	}
 	if data.NextBatch > 0 {
-		//             10        5   总15
+		//             10    		5   总15
+		//page + 1 跳过当前页
 		for i := data.Page + 1; i <= calculateNextPage; i++{
-			output.PrevBatch = append(output.PrevBatch, i)
+			output.NextBatch = append(output.NextBatch, i)
 		}
 	}
 	return
+
 }
+
---
+++ record/paging/test/paging_test.go
@@ -0,0 +1,799 @@
+package paging__test
+
+import (
+	"gcl/paging"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//pageSize
+func TestPageSize(t *testing.T) {
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     0,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageSize, 10)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     20,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageSize, 20)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     -10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageSize, 10)
+	}
+}
+
+//page
+func TestPage(t *testing.T) {
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.Page, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         0,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.Page, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         -1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.Page, 1)
+	}
+}
+
+//pageCount
+func TestPageCount(t *testing.T) {
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageCount, 10)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         2,
+			PageSize:     20,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageCount, 5)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageCount, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    99,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageCount, 10)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    101,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageCount, 11)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    0,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PageCount, 0)
+	}
+}
+
+//DataTotal
+func TestDataTotal(t *testing.T) {
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    0,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.DataTotal, 0)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.DataTotal, 100)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    -1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.DataTotal, 0)
+	}
+}
+//HasPaging
+func TestHasPaging(t *testing.T) {
+	{
+		pagingData := paging.PagingInput{
+			Page:         0,
+			PageSize:     0,
+			DataTotal:    0,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.HasPaging, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.HasPaging, true)
+	}
+}
+
+func TestIsFirstPage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsFirstPage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsFirstPage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsFirstPage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         -20,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsFirstPage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         30,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsFirstPage, false)
+	}
+}
+
+func TestIsLastPage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsLastPage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+
+		assert.Equal(t, output.IsLastPage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsLastPage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         -20,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsLastPage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         30,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.IsLastPage, true)
+	}
+}
+
+
+func TestPrevPage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevPage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         2,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+
+		assert.Equal(t, output.PrevPage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevPage, 9)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         -1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevPage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     20,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevPage, 1)
+	}
+
+}
+
+
+func TestNextPage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextPage, 2)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+
+		assert.Equal(t, output.NextPage, 10)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextPage, 10)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         20,
+			PageSize:     10,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextPage, 10)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     20,
+			DataTotal:    100,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextPage, 5)
+	}
+}
+
+func TestPrevSomePage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 0,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 0)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         2,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         3,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 2)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         8,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 3)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         20,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 5)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         3,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 4,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 1)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevSomePage: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevSomePage, 1)
+	}
+}
+
+func TestNextSomePage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+			NextSomePage: 0,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextSomePage, 0)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+			NextSomePage: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextSomePage, 6)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         -1,
+			PageSize:     10,
+			DataTotal:    100,
+			NextSomePage: 2,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextSomePage, 3)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+			NextSomePage: 2,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextSomePage, 10)
+	}
+}
+
+
+func TestPrevBatch(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch:  5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevBatch, []int{4,5,6,7,8})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevBatch, []int{2,3,4})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevBatch, []int{})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         2,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevBatch, []int{})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         3,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevBatch, []int{2})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     20,
+			DataTotal:    100,
+			PrevBatch:  -1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevBatch, []int{})
+	}
+}
+
+func TestNextBatch(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+			NextBatch:  5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextBatch, []int{2,3,4,5,6})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			NextBatch: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextBatch, []int{6,7,8,9,})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     10,
+			DataTotal:    100,
+			NextBatch: 5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextBatch, []int{})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         9,
+			PageSize:     10,
+			DataTotal:    100,
+			NextBatch: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextBatch, []int{})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         8,
+			PageSize:     10,
+			DataTotal:    100,
+			NextBatch: 1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextBatch, []int{9})
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     20,
+			DataTotal:    100,
+			NextBatch:  -1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.NextBatch, []int{})
+	}
+}
+
+
+func TestPrevHasMorePage(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         8,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch:  5,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 4,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 3,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 2,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         10,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch: 10,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, false)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch:  -1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch:  1,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.PrevHasMorePage, false)
+	}
+}
+
+
+func TestOverall(t *testing.T){
+	{
+		pagingData := paging.PagingInput{
+			Page:         5,
+			PageSize:     10,
+			DataTotal:    100,
+			PrevBatch:  4,
+			NextBatch:  3,
+			PrevSomePage : 1,
+			NextSomePage: 2,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.Page, 5)
+		assert.Equal(t, output.PageSize, 10)
+		assert.Equal(t, output.DataTotal, 100)
+		assert.Equal(t, output.PageCount, 10)
+		assert.Equal(t, output.HasPaging, true)
+		assert.Equal(t, output.IsFirstPage, false)
+		assert.Equal(t, output.IsLastPage, false)
+		assert.Equal(t, output.PrevPage, 4)
+		assert.Equal(t, output.NextPage, 6)
+		assert.Equal(t, output.PrevBatch, []int{2,3,4})
+		assert.Equal(t, output.NextBatch, []int{6,7,8})
+		assert.Equal(t, output.PrevSomePage, 4)
+		assert.Equal(t, output.NextSomePage, 7)
+		assert.Equal(t, output.PrevHasMorePage, false)
+		assert.Equal(t, output.NextHasMorePage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         1,
+			PageSize:     20,
+			DataTotal:    301,
+			PrevBatch:  5,
+			NextBatch:  6,
+			PrevSomePage : 3,
+			NextSomePage: 7,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.Page, 1)
+		assert.Equal(t, output.PageSize, 20)
+		assert.Equal(t, output.DataTotal, 301)
+		assert.Equal(t, output.PageCount, 16)
+		assert.Equal(t, output.HasPaging, true)
+		assert.Equal(t, output.IsFirstPage, true)
+		assert.Equal(t, output.IsLastPage, false)
+		assert.Equal(t, output.PrevPage, 1)
+		assert.Equal(t, output.NextPage, 2)
+		assert.Equal(t, output.PrevBatch, []int{})
+		assert.Equal(t, output.NextBatch, []int{2,3,4,5,6,7})
+		assert.Equal(t, output.PrevSomePage, 1)
+		assert.Equal(t, output.NextSomePage, 8)
+		assert.Equal(t, output.PrevHasMorePage, false)
+		assert.Equal(t, output.NextHasMorePage, true)
+	}
+	{
+		pagingData := paging.PagingInput{
+			Page:         16,
+			PageSize:     20,
+			DataTotal:    305,
+			PrevBatch:  8,
+			NextBatch:  9,
+			PrevSomePage : 15,
+			NextSomePage: 2,
+		}
+		output := paging.Paginate(pagingData)
+		assert.Equal(t, output.Page, 16)
+		assert.Equal(t, output.PageSize, 20)
+		assert.Equal(t, output.DataTotal, 305)
+		assert.Equal(t, output.PageCount, 16)
+		assert.Equal(t, output.HasPaging, true)
+		assert.Equal(t, output.IsFirstPage, false)
+		assert.Equal(t, output.IsLastPage, true)
+		assert.Equal(t, output.PrevPage, 15)
+		assert.Equal(t, output.NextPage, 16)
+		assert.Equal(t, output.PrevBatch, []int{8,9,10,11,12,13,14,15})
+		assert.Equal(t, output.NextBatch, []int{})
+		assert.Equal(t, output.PrevSomePage, 1)
+		assert.Equal(t, output.NextSomePage, 16)
+		assert.Equal(t, output.PrevHasMorePage, true)
+		assert.Equal(t, output.NextHasMorePage, false)
+	}
+}
