39942bae7516bc4e1c50cb673f351d38a4bc1279
phpbulinbulin <18895691876@163.com>
2021-05-26 00:22:17 +0800

    exit goroutine

---
+++ record/exit_goroutine/exit_groutine.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+// n个接收者和一个发送者 通过close通知接收者通道关闭
+
+var wg sync.WaitGroup
+func main()  {
+	jobs := make(chan int, 3)
+	// 开启3个接收者执行任务
+	for i := 0; i < 3; i++ {
+		go receive(jobs)
+	}
+
+	// 1个发送者 发送十个任务
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		jobs <- rand.Intn(10)
+	}
+	close(jobs)
+	wg.Wait()
+
+}
+
+func receive(jobs <-chan int)  {
+	for {
+		value, ok := <- jobs
+		if ok == false {
+			break
+		}
+		fmt.Println(value)
+		wg.Done()
+		// DB insert update delete ...
+	}
+}
---
+++ record/exit_goroutine/exit_groutine_2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main()  {
+	// 1个接收者和n个发送者 通过close通知接收者通道关闭
+	jobs := make(chan int)
+
+	go receive(jobs)
+
+	// 5个 发送者
+	for i := 0; i < 5; i++ {
+		send(jobs)
+	}
+}
+
+func receive(jobs <-chan int)  {
+	// 只处理50个任务
+	limit := 10
+	var completeCount int
+
+	for limit > completeCount{
+		completeCount++
+		value := <- jobs
+		fmt.Println(value)
+	}
+}
+
+func send(jobs chan<- int)  {
+	for {
+		jobs <- rand.Intn(10)
+	}
+}
