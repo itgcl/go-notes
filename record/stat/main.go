24f29c177d77a27c1908a02c94e63d5fd923fb80
phpbulinbulin <18895691876@163.com>
2021-05-27 00:52:58 +0800

    Create main.go

---
+++ record/stat/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	goroutineReceiveCount := 4
+	nums := make(chan int, 1000)
+	results := make(chan int, 1000)
+	exitCh := make(chan struct{}, goroutineReceiveCount)
+	go putNum(nums)
+
+	for i := 0; i < goroutineReceiveCount; i++ {
+		go parseNum(nums, results,exitCh)
+	}
+
+	go func() {
+		for i := 0; i < goroutineReceiveCount ; i++ {
+			<- exitCh
+		}
+		close(results)
+	}()
+	for {
+		value, ok := <-results
+		if ok == false {
+			break
+		}
+		fmt.Println(value)
+	}
+	fmt.Println("结束")
+
+}
+
+func putNum(nums chan<- int)  {
+	for i:= 0; i < 8000; i++ {
+		nums <- i
+	}
+	close(nums)
+}
+
+func parseNum(nums <-chan int, results chan<- int, exitCh chan<- struct{})  {
+	var flag bool
+	for  {
+		value, ok := <-nums
+		if ok == false {
+			break
+		}
+		flag = true
+		for i := 2;  i < value; i++{
+			if value % i == 0 {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			results <- value
+		}
+	}
+	exitCh<- struct{}{}
+}
