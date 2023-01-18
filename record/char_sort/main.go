b1da0dee387bd43acf287efac734cbb7987d7741
itgcl <18895691876@163.com>
2023-01-18 16:59:06 +0800

    move file

---
+++ record/char_sort/main.go
@@ -0,0 +1,11 @@
+package char_sort
+
+type DBConnectParams struct {
+	DriverName string
+	UserName   string
+	Password   string
+	Host       string
+	Port       string
+	DBName     string
+	Charset    string
+}
